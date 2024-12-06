2ebc5fdbc4abc19123458f1feb0f05c70ca1418c
velinamons <velinamons@gmail.com>
2024-12-06 18:54:48 +0200

    Minor change: add 2 blank lines before class definition

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from app.database import Base
 
+
 class User(Base):
     __tablename__ = "users"
 
